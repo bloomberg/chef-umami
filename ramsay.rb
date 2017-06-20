6030f737314fc82718e81879ea6f49f9e89689d2
Ryan Frantz <ryanleefrantz@gmail.com>
2017-06-20 08:35:13 -0400

    Uses ChefDK as a library for all pre-client ops.
    - Removes test-kitchen as a dependency. It was too much overhead and didn't add as much value as I thought.
    - We can use ChefDK as a library to export and push the Policyfile and dependent cookbooks.
    - There may still be a future where ramsay is integrated into test-kitchen. I dunno.

--- ramsay.rb
+++ ramsay.rb
@@ -1,46 +1,33 @@
-#!/opt/chef/embedded/bin/ruby
+#!/opt/chefdk/embedded/bin/ruby # Mac OSX
+####!/opt/chef/embedded/bin/ruby # Linux
 
 # ramsay
-# Quick notes:
-# 1. Using TK because we can take advantage of the plumbing to generate a
-#    temporary sandbox, resolve cookbook deps, load 'em, and then iterate
-#    over them to enumerate all resources for which we'll generate
-#    recommended tests.
-# 2. Using TK also feels right as perhaps this ability to generate an
-#    initial set of tests automatically would be a useful feature *within* TK.
-# NOTE: I learned that ChefDK has Kitchen-like support for Policyfile:
-# https://github.com/chef/chef-dk/blob/9199ddcb6d1504749cbe689347d6b895b0bd889b/lib/kitchen/provisioner/policyfile_zero.rb
-#
-# 20170615
-#  We can create a ChefDK::Policyfile::Uploader object and push the policyfile:
-#  https://github.com/chef/chef-dk/blob/9199ddcb6d1504749cbe689347d6b895b0bd889b/lib/chef-dk/policyfile/uploader.rb#L75-L77
-#  We need to create a PolicyfileLock object:
-#  https://github.com/chef/chef-dk/blob/9199ddcb6d1504749cbe689347d6b895b0bd889b/lib/chef-dk/policyfile_lock.rb
-#  Use the uploader spec for guidance:
-#  https://github.com/chef/chef-dk/blob/9199ddcb6d1504749cbe689347d6b895b0bd889b/spec/unit/policyfile/uploader_spec.rb
-#  CAVEAT: We may need to update the Policyfile to reflect we're using a chef-zero server (i.e. not local files).
 
-require 'awesome_print' # DEBUG
 require 'chef'
-require 'chef/cookbook_loader'
-require 'chef/cookbook_uploader'
 require 'chef_zero/server'
-require 'chef-dk/command/push'
-require 'chef-dk/command/export'
-require 'kitchen'
-require 'kitchen/driver/dummy'
-require 'kitchen/provisioner/chef_zero'
-require 'kitchen/transport/ssh'
-require 'kitchen/verifier/dummy'
-
-### EXPORT
+require 'chef-dk/authenticated_http'
+require 'chef-dk/policyfile/storage_config'
+require 'chef-dk/policyfile/uploader'
+require 'chef-dk/policyfile_lock'
 require 'chef-dk/policyfile_services/export_repo'
+require 'chef-dk/ui'
+require 'fileutils'
 require 'tmpdir' # Extends Dir.
-### EXPORT
 
-# TODO: Test for this existing and so something appropriate.
 policyfile_lock_file = 'Policyfile.lock.json'
+unless File.exist?(policyfile_lock_file)
+  puts
+  puts '!' * 50
+  puts "Unable to locate '#{policyfile_lock_file}'!"
+  puts 'You may need to run `chef install` to generate it.'
+  puts '!' * 50
+  puts
+  exit 1
+end
 
+# Export the cookbook and prepare a chef-zero-compatible directory.
+# We'll use this as a temporary launch pad for things, as needed, akin
+# to test-kitchen's sandbox.
 cookbook_dir = File.basename(Dir.pwd)
 export_root = Dir.mktmpdir('ramsay-')
 # We need the target dir named the same as the source dir so that `chef` commands
@@ -48,118 +35,141 @@ export_root = Dir.mktmpdir('ramsay-')
 # This is because the commands assume they're being run from within a cookbook
 # directory.
 export_path = File.join(export_root, cookbook_dir)
+puts
+puts "#" * 20
+puts "Exporting #{cookbook_dir} to #{export_path}..."
+puts "#" * 20
 export_service = ChefDK::PolicyfileServices::ExportRepo.new(
   policyfile: policyfile_lock_file,
   export_dir: export_path
 )
-
-puts "Exporting #{cookbook_dir} to #{export_path}..."
 export_service.run
-# TODO: Update .chef/config.rb to include a chef_server_url value appropriate
-# for chef-zero so we can load up the config from the file via
-# Chef::Config.from_file("#{export_path}/.chef/config.rb").
-puts "Export Complete!"
+
 chef_config_file = "#{export_path}/.chef/config.rb"
-puts "Updating #{chef_config_file} with some important bits..."
+puts "#" * 20
+puts "Updating the Chef client config at #{chef_config_file}"
+puts "#" * 20
+# Create a fake client cert based on a dummy cert we have laying around.
+fake_client_key = "#{export_path}/ramsay.pem"
+fake_client_key_src = File.join(File.dirname(__FILE__), %w(support ramsay.pem))
+FileUtils.cp(fake_client_key_src, fake_client_key)
+
 File.open(chef_config_file, 'a') do |f|
-  f.puts 'chef_server_url "http://127.0.0.1:8889"'
+  f.puts "chef_server_url 'http://127.0.0.1:8889'"
+  f.puts "cookbook_path ['#{export_path}/cookbook_artifacts']"
+  f.puts "client_key '#{fake_client_key}'"
+  f.puts "node_name 'ramsay-node'"
 end
+# Import the client config.
 Chef::Config.from_file("#{export_path}/.chef/config.rb")
-ap Chef::Config
-
-=begin
-# TODO: Thinking we may want to hake TK get its config from a Ramsay-specific
-# Kitchen config file (i.e. kitchen.ramsay.yml).
-ramsay_driver = Kitchen::Driver::Dummy.new
-ramsay_platform = Kitchen::Platform.new({name: 'ramsay'})
-provisioner_options = {
-  test_base_path: '/tmp/ramsay-test-base-path',
-  kitchen_root: Dir.pwd
-}
-ramsay_provisioner = Kitchen::Provisioner::ChefZero.new(provisioner_options)
-ramsay_suite = Kitchen::Suite.new({name: 'sweet'})
-ramsay_transport = Kitchen::Transport::Ssh.new
-ramsay_verifier = Kitchen::Verifier::Dummy.new
-
-
-instance_options = {
-  suite: ramsay_suite,
-  platform: ramsay_platform,
-  driver: ramsay_driver,
-  provisioner: ramsay_provisioner,
-  transport: ramsay_transport,
-  verifier: ramsay_verifier,
-  state_file: '.kitchen/ramsay.yml'
-}
-
-# The provisioner needs an instance object as it depends on its values (like name).
-instance = Kitchen::Instance.new(instance_options)
-#ramsay_provisioner.create_sandbox
-#sandbox_path = ramsay_provisioner.sandbox_path
-#puts "SANDBOX PATH: #{sandbox_path}" # DEBUG
-puts # DEBUG
-=end
 
 # Run our own chef-zero instance and use the cookbooks in the sandbox to
 # enumerate the resources.
 server = ChefZero::Server.new(port: 8889)
-#server.start_background
+server.start_background
 
-json_args = nil # nil if we want to use Policyfile
-#json_args = {"run_list" => "recipe[system-base]"}
-#client = Chef::Client.new(json_args, local_mode: true)
-#Chef::Config['chef_server_url'] = 'http://127.0.0.1:8889'
-#Chef::Config['client_key'] = "#{sandbox_path}/validation.pem"
-#Chef::Config['node_name'] = 'ramsay-node'
-#Chef::Config['cookbook_path'] = "#{sandbox_path}/cookbook_artifacts"
-#Chef::Config['use_policyfile'] = true
-#Chef::Config['policy_group'] = 'ramsay-group'   # BE CLEVER
-#Chef::Config['policy_name'] = 'ramsay-policy'   # BE CLEVER
+# Instantiate a Chef::Client object.
+client = Chef::Client.new
 
-=begin
-# BEGIN NON-POLICYFILE COOKBOOK UPLOAD APPROACH
-# Instantiate a CookbookLoader and generate CookbookVersion objects for all
-# cookbooks.
-cl = Chef::CookbookLoader.new(Chef::Config.cookbook_path)
-cookbooks = cl.load_cookbooks
-#puts cl.cookbook_names # DEBUG
-# Iterate over the loaded cookbooks and add the Chef::CookbookVersion objects
-# to an array we can feed to Chef::CookbookUploader and push them all at once.
-all_cookbooks = []
-cookbooks.each do |cb_name, cb_version|
-  all_cookbooks << cb_version
-end
-uploader = Chef::CookbookUploader.new(all_cookbooks)
-uploader.upload_cookbooks
-# NOTE: Since we're using Policyfile, we'll need to mimic `chef push` behavior
-# to push the Policyfile.lock.json and all cookbooks to the server.
-# The above code can still be expanded upon for users not using Policyfile.
-# END NON-POLICYFILE COOKBOOK UPLOAD APPROACH
-=end
+# Push the policy, including all dependent cookbooks.
+puts
+puts "#" * 20
+puts "Uploading policyfile '#{policyfile_lock_file}' and related cookbooks..."
+puts "#" * 20
+storage_config = ChefDK::Policyfile::StorageConfig.new.use_policyfile(policyfile_lock_file)
+ui = ChefDK::UI.new
+policyfile_lock_content = IO.read(policyfile_lock_file)
+lock_data = FFI_Yajl::Parser.new.parse(policyfile_lock_content)
+policyfile_lock = ChefDK::PolicyfileLock.new(
+  storage_config,
+  ui: ui
+).build_from_lock_data(lock_data)
+http_client = ChefDK::AuthenticatedHTTP.new(Chef::Config['chef_server_url'])
+policy_group = Chef::Config['policy_group']
+policy_document_native_api = true
+policyfile_uploader = ChefDK::Policyfile::Uploader.new(
+  policyfile_lock,
+  policy_group,
+  ui: ui,
+  http_client: http_client,
+  policy_document_native_api: policy_document_native_api
+)
+policyfile_uploader.upload_policy # Just the policy
+policyfile_uploader.upload
+puts "Policyfile upload complete"
 
-=begin
-# get node information, including node_name, required to get/build node info;
+puts
+puts "#" * 20
+puts "Running chef-client..."
+puts "#" * 20
+# Get node information, including node_name, required to get/build node info;
 # test afterward with `client.node_name`
 client.run_ohai
 client.load_node # from the server
 client.build_node
 client.setup_run_context # /me crosses fingers
 my_resources = {}
+recipe_resources = {}
 client.run_status.run_context.resource_collection.each do |resource|
-  if my_resources.has_key?(resource.to_s)
-    my_resources[resource.to_s] += 1
+  ## Check for dupe resources? Or will Chef merge them during compile time?
+  #if my_resources.has_key?(resource.to_s)
+  #  my_resources[resource.to_s] += 1
+  #else
+  #  my_resources[resource.to_s] = 1
+  #end
+  canonical_recipe = "#{resource.cookbook_name}::#{resource.recipe_name}"
+  if recipe_resources.key?(canonical_recipe)
+    recipe_resources[canonical_recipe] << resource
   else
-    my_resources[resource.to_s] = 1
+    recipe_resources[canonical_recipe] = [resource]
   end
-  puts "\nRESOURCE: '#{resource.to_s}'"
-  puts "NAME:     '#{resource.name}'"
-  puts "TYPE:     '#{resource.class}'"
-  puts "ACTION:   '#{resource.action.to_s}'"
-  puts "FROM:     '#{resource.cookbook_name}::#{resource.recipe_name}'"
+  #puts "\nRESOURCE: '#{resource.to_s}'"
+  #puts "NAME:     '#{resource.name}'"
+  #puts "TYPE:     '#{resource.class}'"
+  #puts "ACTION:   '#{resource.action.to_s}'"
+  #puts "FROM:     '#{resource.cookbook_name}::#{resource.recipe_name}'"
+  #p resource.state
 end
-=end
 
-#ramsay_provisioner.cleanup_sandbox # SOON
+puts "\n\n\n\n\n\n"
+recipe_resources.each do |recipe, resources|
+  (cookbook, recipe) = recipe.split('::')
+  puts "# spec/unit/#{cookbook}/#{recipe}_spec.rb"
+  puts
+  puts "require 'chefspec'"
+  puts
+  puts "describe '#{recipe}' do"
+  puts "  let(:chef_run) { ChefSpec::ServerRunner.converge(described_recipe) }"
+  resources.each do |resource|
+    puts "  "
+    puts "  it '#{resource.action.first}s #{resource.declared_type} \"#{resource.name}\"' do"
+    puts "    expect(chef_run).to #{resource.action.first}_#{resource.declared_type}('#{resource.name}')"
+    state_attrs = resource.state.keys.map {|attr| ":#{attr}"}.join(', ')
+    puts "    expect(resource).to have_state_attrs(#{state_attrs})"
+    puts "  end"
+    #p "#{resource.name}: #{resource.state}" # We need this info to generate tests re: resource state.
+  end
+  puts "end"
+end
 
-# TODO: Ensure we shut down chef-zero (i.e. via at_exit)
+puts "\n\n\n"
+recipe_resources.each do |recipe, resources|
+  (cookbook, recipe) = recipe.split('::')
+  puts "# spec/integration/#{cookbook}/#{recipe}_spec.rb"
+  puts
+  puts "require 'serverspec'"
+  resources.each do |resource|
+    puts "  "
+    puts "  describe #{resource.declared_type}('#{resource.name}') do"
+    puts "    it { should do_something_appropriate_for_this_resource }"
+    puts "  end"
+    #p "#{resource.name}: #{resource.state}" # We need this info to generate tests re: resource state.
+  end
+  puts "end"
+end
+puts "\n\n\n\n\n\n"
+
+# TODO:
+# 1. Ensure we shut down chef-zero (i.e. via at_exit)
+# 2. Clean up the temp directory.
 
